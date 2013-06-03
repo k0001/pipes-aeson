637740665757f455c916d7db887f11c55679d7ad
Renzo Carbonara <gnuk0001@gmail.com>
2013-06-03 20:43:19 -0300

    decodeD working fine

--- src/Control/Proxy/Aeson.hs
+++ src/Control/Proxy/Aeson.hs
@@ -1,43 +1,65 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Control.Proxy.Aeson
-  ( decodeD
+  ( -- * Decoding
+    decodeD
+  , decode
     -- * Types
-  , JSONError(..)
+  , DecodingError(..)
   ) where
 
 import           Control.Exception          (Exception)
+import           Control.Monad              (unless)
 import qualified Control.Proxy              as P
-import qualified Control.Proxy.Attoparsec   as Pa
+import qualified Control.Proxy.Attoparsec   as PA
 import qualified Control.Proxy.Trans.Either as P
 import qualified Control.Proxy.Trans.State  as P
 import qualified Data.Aeson                 as Ae
-import qualified Data.ByteString            as B
+import qualified Data.ByteString.Char8      as B
+import           Data.Char                  (isSpace)
 import           Data.Data                  (Data, Typeable)
 
 --------------------------------------------------------------------------------
 
-data JSONError
-  = ParserError Pa.ParsingError
+-- | An error that while decoding a JSON value.
+data DecodingError
+  = ParserError PA.ParsingError
+    -- ^An Attoparsec error that happened while parsing the raw JSON string.
   | ValueError String
+    -- ^An Aeson error that happened while trying to convert a 'Ae.Value' to
+    --  an 'Ae.FromJSON' instance.
   deriving (Show, Eq, Data, Typeable)
 
-instance Exception JSONError
+instance Exception DecodingError
 
 --------------------------------------------------------------------------------
 
-decodeD
+decode
   :: (Monad m, P.Proxy p, Ae.FromJSON r)
-  => ()
-  -> P.EitherP JSONError (P.StateP [B.ByteString] p)
+  => P.EitherP DecodingError (P.StateP [B.ByteString] p)
      () (Maybe B.ByteString) y' y m r
-decodeD = \() -> do
-    ev <- P.liftP . P.runEitherP $ Pa.parse Ae.json' ()
+decode = do
+    ev <- P.liftP . P.runEitherP $ PA.parse Ae.json'
     case ev of
       Left e  -> P.throw (ParserError e)
       Right v -> do
           case Ae.fromJSON v of
             Ae.Error e   -> P.throw (ValueError e)
             Ae.Success r -> return r
+{-# INLINABLE decode #-}
+
+
+decodeD
+  :: (Monad m, P.Proxy p, Ae.FromJSON b)
+  => ()
+  -> P.Pipe (P.EitherP DecodingError (P.StateP [B.ByteString] p))
+     (Maybe B.ByteString) b m ()
+decodeD = \() -> loop
+  where
+    loop = do
+        eof <- P.liftP $ PA.isEndOfUsefulParserInput isSpace
+        unless eof $ do
+          () <- P.respond =<< decode
+          loop
+{-# INLINABLE decodeD #-}
 
