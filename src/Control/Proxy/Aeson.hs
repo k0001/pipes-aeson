e3d38fc60f3c23b20c8aa70fd961fc832430a8ea
Renzo Carbonara <gnuk0001@gmail.com>
2013-06-04 01:54:17 -0300

    encode and decode

--- src/Control/Proxy/Aeson.hs
+++ src/Control/Proxy/Aeson.hs
@@ -9,21 +9,29 @@
 module Control.Proxy.Aeson
   ( -- * Decoding
     -- $decoding
-    decodeD
-  , decode
-    -- * Types
+    decode
+  , decodeD
+    -- * Encoding
+    -- $encoding
+  , encode
+  , encodeD
+    -- * Errors
   , DecodingError(..)
   ) where
 
-import           Control.Exception          (Exception)
-import           Control.Monad              (unless)
-import qualified Control.Proxy              as P
-import qualified Control.Proxy.Attoparsec   as PA
-import qualified Control.Proxy.Trans.Either as P
-import qualified Control.Proxy.Trans.State  as P
-import qualified Data.Aeson                 as Ae
-import qualified Data.ByteString.Char8      as B
-import           Data.Data                  (Data, Typeable)
+import           Control.Exception             (Exception)
+import           Control.Monad                 (unless)
+import qualified Control.Proxy                 as P
+import qualified Control.Proxy.Attoparsec      as PA
+import qualified Control.Proxy.Parse           as Pp
+import qualified Control.Proxy.Trans.Either    as P
+import qualified Control.Proxy.Trans.State     as P
+import qualified Data.Aeson                    as Ae
+import qualified Data.ByteString.Char8         as B
+import qualified Data.ByteString.Lazy.Internal as BLI
+import qualified Data.Char                     as Char
+import           Data.Data                     (Data, Typeable)
+import           Data.Function                 (fix)
 
 --------------------------------------------------------------------------------
 
@@ -41,7 +49,7 @@ instance Exception DecodingError
 --------------------------------------------------------------------------------
 -- $decoding
 --
--- There are different JSON decoding facilities exported by this module, and
+-- There are two different JSON decoding facilities exported by this module, and
 -- choosing between them is easy: If you need to interleave JSON decoding
 -- with other stream effects you must use 'decode', otherwise you may use the
 -- simpler 'decodeD'.
@@ -53,23 +61,22 @@ instance Exception DecodingError
 
 -- | Decodes one JSON value flowing downstream.
 --
--- * This proxy is meant to be composed in the 'P.request' category.
---
 -- * In case of decoding errors, a 'DecodingError' exception is thrown in
 -- the 'Pe.EitherP' proxy transformer.
 --
 -- * Requests more input from upstream using 'Pa.draw' when needed.
 --
--- * /Do not/ use this proxy if 'PA.isEndOfNonWhitespaceParserInput' returns
--- 'True', otherwise you may get unexpected boundary errors.
+-- * /Do not/ use this proxy if your stream has leading empty chunks or
+-- whitespace, otherwise you may get unexpected parsing errors.
 --
 -- Here is an example parsing loop that allows interleaving stream effects
 -- together with 'decode':
 --
 -- > loop = do
--- >     eof <- liftP isEndOfNonWhitespaceParserInput
+-- >     -- Skip any leading whitespace and check that we haven't reached EOF.
+-- >     eof <- liftP $ skipSpace >> isEndOfParserInput
 -- >     unless eof $ do
--- >         -- 1. Possibly perform stream some effects here.
+-- >         -- 1. Possibly perform some stream effects here.
 -- >         -- 2. Decode one JSON element from the stream.
 -- >         exampleElement <- decode
 -- >         -- 3. Do something with exampleElement and possibly perform
@@ -91,10 +98,7 @@ decode = do
 {-# INLINABLE decode #-}
 
 
--- | Decodes consecutive JSON values flowing downstream until
--- 'PA.isEndOfNonWhitespaceParserInput'.
---
--- * This proxy is meant to be composed in the 'P.pull' category.
+-- | Decodes consecutive JSON values flowing downstream until end of input.
 --
 -- * In case of decoding errors, a 'DecodingError' exception is thrown in
 -- the 'Pe.EitherP' proxy transformer.
@@ -111,9 +115,53 @@ decodeD
 decodeD = \() -> loop
   where
     loop = do
-        eof <- P.liftP PA.isEndOfNonWhitespaceParserInput
-        unless eof $ do
-          () <- P.respond =<< decode
-          loop
+        eof <- P.liftP $ skipSpace >> PA.isEndOfParserInput
+        unless eof $ decode >>= P.respond >> loop
 {-# INLINABLE decodeD #-}
 
+--------------------------------------------------------------------------------
+-- $encoding
+--
+-- There are two different JSON encoding facilities exported by this module, and
+-- choosing between them is easy: If you need to interleave JSON encoding
+-- with other stream effects you must use 'encode', otherwise you may use the
+-- simpler 'encodeD'.
+
+
+-- | Encodes the given 'Ae.ToJSON' instance and sends it downstream, possibly in
+-- more than one 'BS.ByteString' chunks.
+encode
+  :: (P.Proxy p, Monad m, Ae.ToJSON r)
+  => r -> p x' x () B.ByteString m ()
+encode = \r -> P.runIdentityP $ do
+    BLI.foldrChunks (\e a -> P.respond e >> a) (return ()) (Ae.encode r)
+{-# INLINABLE encode #-}
+
+
+-- | Encodes 'Ae.ToJSON' instances flowing downstream, each in possibly more
+-- than one 'BS.ByteString' chunks.
+encodeD
+  :: (P.Proxy p, Monad m, Ae.ToJSON a)
+  => () -> P.Pipe p a B.ByteString m r
+encodeD = P.pull P./>/ encode
+{-# INLINABLE encodeD #-}
+
+--------------------------------------------------------------------------------
+
+-- | Consume and discard leading 'I.ParserInput' characters from upstream as
+-- long as the given predicate holds 'True'.
+
+-- XXX: we define 'skipSpace' here until 'pipes-bytestring' is released.
+skipSpace
+  :: (Monad m, P.Proxy p)
+  => P.StateP [B.ByteString] p () (Maybe B.ByteString) y' y m ()
+skipSpace = fix $ \loop -> do
+    ma <- Pp.draw
+    case ma of
+      Nothing -> return ()
+      Just a  -> do
+        let a' = B.dropWhile Char.isSpace a
+        if B.null a'
+           then loop
+           else Pp.unDraw a'
+{-# INLINABLE skipSpace #-}
