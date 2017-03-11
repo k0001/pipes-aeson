9a38405cc1df02d6dbaefbc9eab0b958df5b669f
Renzo Carbonara <gnuk0001@gmail.com>
2017-03-11 13:48:00 +0100

    nix files

--- default.nix
+++ default.nix
@@ -1,16 +1,1 @@
-{ mkDerivation, aeson, attoparsec, base, bytestring, pipes
-, pipes-attoparsec, pipes-bytestring, pipes-parse, stdenv
-, transformers
-}:
-mkDerivation {
-  pname = "pipes-aeson";
-  version = "0.4.1.8";
-  src = ./.;
-  libraryHaskellDepends = [
-    aeson attoparsec base bytestring pipes pipes-attoparsec
-    pipes-bytestring pipes-parse transformers
-  ];
-  homepage = "https://github.com/k0001/pipes-aeson";
-  description = "Encode and decode JSON streams using Aeson and Pipes";
-  license = stdenv.lib.licenses.bsd3;
-}
+(import ./release.nix {}).pipes-aeson
